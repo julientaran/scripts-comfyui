b27c645cd99752d440a41dfcfd482fe1691d9f1d
julien_taran <julientaran.imessage@gmail.com>
2024-12-10 22:05:19 -0600

    Slight changes

---
+++ global_pullid.sh
@@ -0,0 +1,4 @@
+./venv_p39.sh
+./pull_id_install.sh
+./download_flux.sh
+./download_models.sh
--- pull_id_install.sh
+++ pull_id_install.sh
@@ -1,5 +1,8 @@
+echo "##### STARTED PULL ID INSTALL SCRIPT ##########"
 cd ComfyUI/custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+python -m pip install -r requirements.txt
+cd ../..
 python -m pip install --use-pep517 facexlib
 python -m pip install insightface onnxruntime
-
+echo "########## PULL ID INSTALL SCRIPT FINISHED ##########"
