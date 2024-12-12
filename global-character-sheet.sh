7a6baa78e31685e3fa4475534939c7796bbf5b94
julien_taran <julientaran.imessage@gmail.com>
2024-12-12 09:55:46 -0600

    Added req and starting nez script

---
+++ global-character-sheet.sh
@@ -0,0 +1,7 @@
+python -m venv 00-env
+source 00-env/bin/activate
+git
+git clone https://github.com/comfyanonymous/ComfyUI.git
+cd ComfyUI/custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+git clone 
