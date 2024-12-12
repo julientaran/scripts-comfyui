c70ff4cd9438ef316f38df16b657549f1a996b7f
julien_taran <julientaran.imessage@gmail.com>
2024-12-11 19:40:53 -0600

    Legeres modifs

--- .download_models_old.sh
+++ .download_models_old.sh
@@ -4,23 +4,12 @@
 MODEL_INFO=(
     "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors ComfyUI/models/clip/clip_l.safetensors"
     "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors ComfyUI/models/clip/t5xxl_fp16.safetensors"
-    "https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors ComfyUI/models/vae/ae.safetensors"
+    "https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors ComfyUI/models/vae/ae.safetensors"
     "https://huggingface.co/InstantX/FLUX.1-dev-Controlnet-Union/resolve/main/diffusion_pytorch_model.safetensors ComfyUI/models/controlnet/diffusion_pytorch_models.safetensors"
     "https://huggingface.co/skbhadra/ClearRealityV1/resolve/main/4x-ClearRealityV1.pth ComfyUI/models/upscale_models/4x-ClearRealityV1.pth"
+    "https://huggingface.co/guozinan/PuLID/resolve/main/pulid_flux_v0.9.0.safetensors ComfyUI/models/pulid/pulid_flux_v0.9.0.safetensors"
+    "https://huggingface.co/MonsterMMORPG/tools/resolve/main/antelopev2.zip ComfyUI/models/insightface/models/antelopev2.zip"
 )
-
-# Modele additionnel pour le mode pull_id
-PULL_ID_MODEL="https://huggingface.co/special_model/resolve/main/additional_model.safetensors ComfyUI/models/special/additional_model.safetensors"
-
-# Demander a l'utilisateur s'il souhaite activer le mode pull_id
-read -p "Voulez-vous activer le mode pull_id (oui/non) ? " PULL_ID_MODE
-
-if [[ "$PULL_ID_MODE" =~ ^[Oo][Uu][Ii]$ || "$PULL_ID_MODE" =~ ^[Yy][Ee][Ss]$ ]]; then
-    echo "Mode pull_id active. Ajout d'un modele supplementaire."
-    MODEL_INFO+=("$PULL_ID_MODEL")
-else
-    echo "Mode pull_id non active. Installation standard."
-fi
 
 # Installation des modeles
 for MODEL in "${MODEL_INFO[@]}"; do
